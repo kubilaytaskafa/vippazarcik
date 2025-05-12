8f56e98623cd949eb2b3f2272b7989236f4ce6b9
kubilay taşkafa <info.kubilay0@gmail.com>
2025-05-12 07:38:15 +0300

    vippazarcik-1.0.5

--- src/components/CarGallery.jsx
+++ src/components/CarGallery.jsx
@@ -1,70 +1,22 @@
 import { useSelector } from "react-redux";
-import { useState } from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const CarGallery = () => {
   const cars = useSelector((state) => state.cars.cars);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === cars.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? cars.length - 1 : prevIndex - 1
-    );
-  };
 
   return (
-    <div className="relative">
-      <div className="overflow-hidden rounded-lg">
-        <div
-          className="flex transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-        >
-          {cars.map((car) => (
-            <div
-              key={car.id}
-              className="w-full flex-shrink-0"
-              style={{ height: "500px" }}
-            >
-              <img
-                src={car.image}
-                alt={car.name}
-                className="w-full h-full object-cover"
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Navigation Buttons */}
-      <button
-        onClick={prevSlide}
-        className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/80 p-2 rounded-full hover:bg-white transition-colors"
-      >
-        <FaChevronLeft className="text-2xl text-primary" />
-      </button>
-      <button
-        onClick={nextSlide}
-        className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/80 p-2 rounded-full hover:bg-white transition-colors"
-      >
-        <FaChevronRight className="text-2xl text-primary" />
-      </button>
-
-      {/* Dots */}
-      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex space-x-2">
-        {cars.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => setCurrentIndex(index)}
-            className={`w-3 h-3 rounded-full transition-colors ${
-              index === currentIndex ? "bg-primary" : "bg-white/50"
-            }`}
-          />
+    <div className="w-full">
+      <div className="flex flex-wrap gap-4 justify-center">
+        {cars.map((car) => (
+          <div
+            key={car.id}
+            className="w-[300px] h-[200px] rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300"
+          >
+            <img
+              src={car.image}
+              alt={car.name}
+              className="w-full h-full object-cover"
+            />
+          </div>
         ))}
       </div>
     </div>
